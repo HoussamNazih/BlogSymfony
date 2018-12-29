b66ed637d520733d63f2f2ca8ecee7e44b3b7eff
Nazih Houssam <houssam.nazih@etu.umontpellier.fr>
2018-12-29 03:15:25 +0100

    update

--- src/AppBundle/Form/PostType.php
+++ src/AppBundle/Form/PostType.php
@@ -18,9 +18,11 @@ class PostType extends AbstractType
         $builder
             ->add('Titre')
             ->add('image', FileType::class, array('label'=>'inserer une image'))
-            ->add('contenu', CKEditorType::class, array(
+            ->add('contenu', CKEditor::class, array(
                 'config' => array(
                     'uiColor' => '#ffffff',
+                    'base_path' => 'ckeditor',
+                    'js_path'   => 'ckeditor/ckeditor.js'
                     //...
                 ),
             ))
